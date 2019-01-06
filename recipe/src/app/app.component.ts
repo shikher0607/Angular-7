f852eaaa64b5b174d6ce0dd49686ae4ca263a6f5
shikher0607 <shikher06071994@gmail.com>
2019-01-06 19:10:18 +0530

    Recipe-App-with-Components-DataBinding-Part1

--- recipe/src/app/app.component.ts
+++ recipe/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loadedFeature: string = 'recipe';
+
+  onNavigation(headerFeature){
+    this.loadedFeature = headerFeature;
+  }
   
 }
