9998909fb5d53590bd3027fda4f4cd2fcf468cfb
shikher0607 <shikher06071994@gmail.com>
2019-01-08 23:40:37 +0530

    Directives-PartII-&-RecipeApp

--- directives-start/directives-start/src/app/app.module.ts
+++ directives-start/directives-start/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { BasichighLightDirective } from './basic-highlight/basic-highlight.directive';
 import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
+import { UnlessDirective } from './unless.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     BasichighLightDirective,
-    BetterHighlightDirective
+    BetterHighlightDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
--- directives-start/directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ directives-start/directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,25 +1,29 @@
-import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @Input('appBetterHighlight') defaultColor: string = 'transparent';
+  @Input() highlightColor: string = 'blue';
+
   @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
  
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(){
    // this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'blue');
+   this.backgroundColor = this.defaultColor;
   }
 
   @HostListener('mouseenter') mouseover(eventData: Event){
    // this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'blue');
-    this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event){
     //this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'transparent');
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
   }
 
 }
