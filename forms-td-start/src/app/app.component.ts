91318cb452cf31b946571650b60fd007fdf73309
shikher0607 <shikher06071994@gmail.com>
2019-01-30 13:32:56 +0530

    Angular Forms

---
+++ forms-td-start/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild('f') signupForm: NgForm;
+  defaultQuestion = 'pet';
+  answer = '';
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+
+  /* onSubmit(form: NgForm){
+    console.log(form);
+  } */
+
+  onSubmit(){
+    console.log(this.signupForm);
+  }
+}
