6f7b5c2aa1f8ea5fdeac0594ace3f94d92311976
shikher0607 <shikher06071994@gmail.com>
2019-02-01 18:30:27 +0530

    Recipe with Forms

--- recipe/src/app/recipes/recipe.service.ts
+++ recipe/src/app/recipes/recipe.service.ts
@@ -2,9 +2,11 @@ import { Injectable } from '@angular/core';
 import {Recipe} from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class RecipeService{
+    recipeChanged = new Subject<Recipe[]>();
 
     constructor(private shoppingListService: ShoppingListService){}
 
@@ -29,5 +31,20 @@ export class RecipeService{
 
       addIngredientsToSl(ingredients: Ingredient[]){
         this.shoppingListService.addIngredients(ingredients);
+      }
+
+      addRecipe(recipe: Recipe){
+        this.recipes.push(recipe);
+        this.recipeChanged.next(this.recipes.slice());
+      }
+
+      updateRecipe(index: number, newRecipe: Recipe){
+          this.recipes[index] = newRecipe;
+          this.recipeChanged.next(this.recipes.slice());
+      }
+
+      deleteRecipe(index: number){
+        this.recipes.splice(index, 1);
+        this.recipeChanged.next(this.recipes.slice());
       }
 }
